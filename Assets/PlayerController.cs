51103b8b7995226d0484c3748233ffe3b4dc7e31
gox <gox.roche@gmail.com>
2020-09-02 19:38:28 +0200

    first commit

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UDPSend;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody rb;
+    private Vector3 lastPosition;
+    public Quaternion lastRotation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        transform.LookAt(Vector3.zero);
+        lastPosition = transform.position;
+        lastRotation = transform.rotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveCharacter();
+        FocusMouseCursor();
+        SendLastPlayerPosition();
+    }
+
+    void MoveCharacter()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rb.AddForce(movement * speed);
+    }
+
+    void FocusMouseCursor()
+    {
+        Vector3 mousePosition = Input.mousePosition;
+        Vector3 playerPosition = transform.position;
+
+        Ray ray = Camera.main.ScreenPointToRay(mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, 1000))
+        {
+            Vector3 hitpoint = hit.point;
+            transform.LookAt(hitpoint);
+            Vector3 targetPostition = new Vector3(hitpoint.x, this.transform.position.y, (hitpoint.z));
+            this.transform.LookAt(targetPostition);
+
+        }
+    }
+
+    void SendLastPlayerPosition()
+    {
+        if (!transform.position.Equals(lastPosition) || !transform.rotation.Equals(lastRotation))
+        {
+            UDPSender.getInstance().sendString(transform.position + " " + transform.rotation);
+            Debug.Log("Send UDP msg");
+        }
+
+        lastPosition = transform.position;
+        lastRotation = transform.rotation;
+    }
+}
---
+++ Assets/UDPSend.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace UDPSend
+{
+    public class UDPSender {
+
+        private static UDPSender instance;
+        private static int localPort;
+        private string IP; 
+        public int port;
+        IPEndPoint remoteEndPoint;
+        UdpClient client;
+
+        private UDPSender()
+        {
+            Debug.Log("UDPSend.init()");
+            IP = "127.0.0.1";
+            port = 4445;
+            remoteEndPoint = new IPEndPoint(IPAddress.Parse(IP), port);
+            client = new UdpClient();
+        }
+
+        // sendData
+        public void sendString(string message)
+        {
+            try
+            {
+                byte[] data = Encoding.UTF8.GetBytes(message);
+                client.Send(data, data.Length, remoteEndPoint);
+            }
+            catch (Exception err)
+            {
+                Debug.Log(err.ToString());
+            }
+        }
+
+        public static UDPSender getInstance()
+        {
+            if(instance == null)
+            {
+                instance = new UDPSender();
+            }
+            return instance;
+        }
+    }
+}
